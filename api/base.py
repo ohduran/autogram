e24c7ff40c44cd0eabf8018e61ad5fe0b422a6a1
ohduran <alvaro.duranb@gmail.com>
2019-11-22 18:16:52 +0100

    control for errors when uploading a picture

--- api/base.py
+++ api/base.py
@@ -42,12 +42,14 @@ class InstagramBot(InstagramAPI):
 Great 📸 by {owner}! Thanks for sharing\n\n\n\n\n\n\n\n\n\n\n\n
 #barcelona #sightsofbcn #bcn\n\n\n
 """.format(caption=caption.text, owner=str(picture.owner))
-        self.uploadPhoto(picture.path, caption_full_text, upload_id=None, is_sidecar=None)
-        log.info(f'Picture by {picture.owner} uploaded')
-        log.info(f'Caption used: {caption.text}')
-        picture.times_used = F('times_used') + 1
-        picture.save()
+        if self.uploadPhoto(picture.path, caption_full_text, upload_id=None, is_sidecar=None):
+            log.info(f'Picture by {picture.owner} uploaded')
+            log.info(f'Caption used: {caption.text}')
+            picture.times_used = F('times_used') + 1
+            picture.save()
 
-        caption.times_used = F('times_used') + 1
-        caption.save()
-        log.info('Picture uploaded!')
+            caption.times_used = F('times_used') + 1
+            caption.save()
+            log.info('Picture uploaded!')
+        else:
+            log.info(f'There was an error and the picture could not be loaded')
